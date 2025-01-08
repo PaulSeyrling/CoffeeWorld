4c94d7921b8af468d023911a2c46a597e3595764
PaulSeyrling <171835817+PaulSeyrling@users.noreply.github.com>
2025-01-08 11:35:23 +0100

    revoke

--- api/coffee.js
+++ api/coffee.js
@@ -1,11 +1,11 @@
 // api/coffee.js
-const coffeeData = require('../../data/coffeeData');
+const coffeeData = require('../data/coffeeData');
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-
+  
   // Handle OPTIONS request for CORS preflight
   if (req.method === 'OPTIONS') {
     res.status(200).end();
@@ -19,16 +19,15 @@ export default async function handler(req, res) {
       // Return specific coffee by ID
       const coffee = coffeeData.find(coffee => coffee.id === id);
       if (coffee) {
-        res.status(200).json(coffee);
+        res.json(coffee);
       } else {
         res.status(404).json({ error: "Coffee not found" });
       }
     } else {
       // Return all coffees if no ID is specified
-      res.status(200).json(coffeeData);
+      res.json(coffeeData);
     }
   } else {
     res.status(405).json({ error: "Method not allowed" });
   }
-}
-
+};
