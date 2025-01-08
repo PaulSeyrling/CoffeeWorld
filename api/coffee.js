4160aa54d141a571a077bc35c5d1850943e3b2bc
PaulSeyrling <171835817+PaulSeyrling@users.noreply.github.com>
2025-01-08 11:33:49 +0100

    Data Updated

--- api/coffee.js
+++ api/coffee.js
@@ -1,11 +1,11 @@
 // api/coffee.js
-const coffeeData = require('../data/coffeeData');
+const coffeeData = require('../../data/coffeeData');
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  
+
   // Handle OPTIONS request for CORS preflight
   if (req.method === 'OPTIONS') {
     res.status(200).end();
@@ -19,15 +19,16 @@ module.exports = async (req, res) => {
       // Return specific coffee by ID
       const coffee = coffeeData.find(coffee => coffee.id === id);
       if (coffee) {
-        res.json(coffee);
+        res.status(200).json(coffee);
       } else {
         res.status(404).json({ error: "Coffee not found" });
       }
     } else {
       // Return all coffees if no ID is specified
-      res.json(coffeeData);
+      res.status(200).json(coffeeData);
     }
   } else {
     res.status(405).json({ error: "Method not allowed" });
   }
-};
+}
+
